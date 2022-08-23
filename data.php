82bc618f0eaad8c57e10fcf6ea13d3066f1d1032
unknown <shamvil89@gmail.com>
2022-08-23 21:54:16 +0600

    redis chatroom

---
+++ data.php
@@ -0,0 +1,25 @@
+<?php 
+require "vendor/autoload.php";
+use Predis\Client;
+$redis = new Client();
+
+if(isset($_POST['message'])){
+    $sender = $_POST['sender'];
+    $message = $_POST['message'];
+    $redis->rpush('chatroom-001',$sender . ": ".$message);
+    $messages =  $redis->lrange('chatroom-001',0,$redis->llen('chatroom-001'));
+    echo "<p>";
+    foreach($messages as $m){
+        echo $m."<br/>";
+    }
+    echo "</p>";
+}
+
+if(isset($_POST['refresh'])){
+    $messages =  $redis->lrange('chatroom-001',0,$redis->llen('chatroom-001'));
+    echo "<p>";
+    foreach($messages as $m){
+        echo $m."<br/>";
+    }
+    echo "</p>";
+}
